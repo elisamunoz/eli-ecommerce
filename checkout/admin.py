a907b7fd2458437f5bc36dfdff29a7dfbad5c11c
Elisa Munoz <elisamunozes@gmail.com>
2020-04-26 18:23:49 +0100

    checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineAdminInline(admin.TabularInline):  # TabularInline subclass defines the template used to render the Order in the admin interface. StackedInline is another option
+    model = OrderLineItem
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineAdminInline, )
+
+
+admin.site.register(Order, OrderAdmin)
+
